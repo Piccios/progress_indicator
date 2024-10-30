6787f90d9574f50d65eea4a6080519ea8b264f69
Piccio <lorenzo.hl.picchi@gmail.com>
2024-10-30 17:42:41 +0100

    Implement dynamic reading progress bar: calculates scroll percentage and updates bar width with `scrollPercentage`

---
+++ script.js
@@ -0,0 +1,7 @@
+window.addEventListener("scroll", () => {
+    const scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+    const scrollHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    const scrollPercentage = (scrollTop / scrollHeight) * 100;
+
+    document.querySelector(".progression-bar").style.width = scrollPercentage + "%";
+});
